06d253e1463f4d7740f7368ee46255b08331e7b5
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-28 18:26:30 -0600

    create action in uploads_controller

--- app/controllers/csv_loader_controller.rb
+++ app/controllers/csv_loader_controller.rb
@@ -1,8 +1,15 @@
 class CsvLoaderController < ApplicationController
-  before_action :check_authorization
+  before_action :check_authorization, only: [:show]
 
   def show
     @user = current_user
+  end
+
+  def create
+    file_path_to_save_to = './tmp/file.csv'
+    File.write(file_path_to_save_to, params[:file].tempfile.read)
+    CsvImporterJob.perform_later(file_path_to_save_to)
+    redirect_to dashboard_index_path, notice: "File Uploaded. A confirmation email will be sent upon completion."
   end
 
   def check_authorization
