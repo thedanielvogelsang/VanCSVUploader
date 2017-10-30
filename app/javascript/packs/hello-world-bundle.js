dec03302dfea8b020c1bcea4994ab77ebf1b3b05
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-30 12:36:22 -0600

    changes accept.jsx class syntax to export

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -2,8 +2,8 @@ import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 import HomeMessage from '../home'
-
+import Accept from '../accept'
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld, HomeMessage
+  HelloWorld, HomeMessage, Accept
 });
