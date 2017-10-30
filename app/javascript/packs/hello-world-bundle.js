27a36543ef2b3275dc5640e6ee2bd7d8c5e7e921
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-30 13:07:22 -0600

    puts dropzoneplace into action in index.html.erb

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -3,8 +3,8 @@ import ReactOnRails from 'react-on-rails';
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 import HomeMessage from '../home'
 import Accept from '../accept'
-import Dropzone from '../dropzone'
+import DropZonePlace from '../dropzone'
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld, HomeMessage, Accept, Dropzone
+  HelloWorld, HomeMessage, Accept, DropZonePlace
 });
