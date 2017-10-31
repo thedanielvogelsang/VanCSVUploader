a5bf7241e3848923826a37dc1693ae2f64e52ef7
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-31 17:58:51 -0600

    homepage entirely react now

--- app/javascript/button.jsx
+++ app/javascript/button.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 
 export default class Button extends React.Component {
   constructor(props, _railsContext){
-    super(props)
-    
+    super(props);
+    this.state = {
+      style: {background: 'none',
+              border: 'none'}
+    };
   }
   render(){
     return (
-      <button className={this.props.text}>
-        <a href={this.props.path} >{this.props.text.capitalize}</a
+      <button
+        className={this.props.path}
+        style={this.state.style}
+        >
+        <a href={'/'+this.props.path} >{this.props.text}</a>
       </button>
     )
   }
--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,10 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import HomeMessage from '../home'
-import Accept from '../accept'
-import DropZonePlace from '../dropzone'
-import Button from '../button'
+import HomeMessage from '../home';
+import Accept from '../accept';
+import DropZonePlace from '../dropzone';
+import Button from '../button';
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld, HomeMessage, Accept, DropZonePlace, Button
