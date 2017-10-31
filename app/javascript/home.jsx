d0530a6284d2066f3a348e03ce72ae6f88360498
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-31 16:05:09 -0600

    upload confidently text on homepage

--- app/javascript/home.jsx
+++ app/javascript/home.jsx
@@ -3,16 +3,12 @@ import React from 'react';
 export default class HomeMessage extends React.Component {
   constructor(props, _railsContext) {
     super(props);
-    this.state = {
-      display: ''
-    };
-    this.checkStatus = this.checkStatus.bind(this);
+
   }
 
   render() {
-    this.checkStatus
     return (
-      <h1 style={{background: this.state.display}}>{this.props.text}</h1>
+      <h1>{this.props.text}</h1>
     );
   }
 }
