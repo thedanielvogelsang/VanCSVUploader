9d412fccc76ee63596a5a581fdd5e1cfb7efd831
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-11-01 09:40:14 -0600

    page stylings updated for presentation

--- app/javascript/home.jsx
+++ app/javascript/home.jsx
@@ -11,14 +11,20 @@ export default class HomeMessage extends React.Component {
     return (
       <div className='homepage-body'>
        <div className='homepage-container-1'>
-        <h1>{this.props.header}</h1>
+        <div className='header-div'>
+          <h1>{this.props.header}</h1>
+        </div>
         <div className='subheader-div'>
           <h3>{this.props.sub1}</h3>
           <h2>{this.props.logo}</h2>
           <h3>{this.props.sub2}</h3>
         </div>
-        <Button text='Sign Up' path='sign-up'/>
-        <Button text='Sign In' path='login'/>
+        <div className='button-div'>
+          <Button text='Sign Up' path='sign-up'/>
+          <Button text='Sign In' path='login'/>
+        </div>
+       </div>
+       <div class='page-footerbar'>
        </div>
       </div>
     );
